7b17c1281fba2f3be608f69faa2018d4531772f7
Daniel <den218den@gmail.com>
2019-02-16 21:53:14 +0300

    week4

---
+++ week4/common.h
@@ -0,0 +1,13 @@
+typedef struct _test_struct{
+
+    char name[32];
+    unsigned int age;
+    char group[16];
+} test_struct_t;
+
+
+typedef struct result_struct_{
+
+    char result[128];
+
+} result_struct_t;
