6549f6a7280e2a18dc53b4784b971e990d3034e4
Daniel <den218den@gmail.com>
2019-02-16 22:08:38 +0300

    server updated

--- week4/client.c
+++ week4/client.c
@@ -7,7 +7,7 @@
 #include "common.h"
 
 #define DEST_PORT            2000
-#define SERVER_IP_ADDRESS   "127.0.0.1"
+#define SERVER_IP_ADDRESS   "192.168.2.1"
 
 test_struct_t client_data;
 result_struct_t result;
